30a6e164d8b8bdd4c12b9026c77cd30160a84fdd
Belkevich_Andru <andromedius@mail.ru>
2024-11-04 16:45:45 +0500

    Home work 9.1 Poetry

---
+++ scr/masks.py
@@ -0,0 +1,18 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Принимает на вход номер карты в формате - 7000792289606361,
+    и возвращает ее маску 7000 79** **** 6361."""
+
+    return card_number[:4] + " " + card_number[4:6] + "**" + " **** " + card_number[11:16]
+
+
+print(get_mask_card_number(str(input("Введите номер карты"))))
+
+
+def get_mask_account(bank_account_number: str) -> str:
+    """Принимает на вход номер счета в формате- 73654108430135874305 и
+    возвращает его маску **4305."""
+
+    return "**" + bank_account_number[-4:]
+
+
+print(get_mask_account(str(input("Введите номер счёта"))))
